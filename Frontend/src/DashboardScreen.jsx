bcb59bc4ef9ba6eb6937569754919226b9b7ecba
Lukachiti <chitidze.luka@outlook.com>
2026-06-12 22:16:13 +0400

    gabrieli

--- Frontend/src/DashboardScreen.jsx
+++ Frontend/src/DashboardScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { levelsData } from "./quizData";
+import {CustomLevelData} from "./customLevelData";
 
 export default function DashboardScreen() {
   const [scores, setScores] = useState({});
@@ -17,27 +18,59 @@ export default function DashboardScreen() {
         <p>აირჩიეთ სირთულის დონე. დასრულებული დონის გადაბარება შეუძლებელია!</p>
       </div>
 
-      <div className="levels-grid">
-        {Object.keys(levelsData).map((lvl) => {
-          const isCompleted = scores[lvl] !== undefined;
-          const displayScore = scores[lvl];
+      <div className="titlee">
+        <h2>Default Levels</h2>
+        <div className="levels-grid">
+          {Object.keys(levelsData).map((lvl) => {
+            const isCompleted = scores[lvl] !== undefined;
+            const displayScore = scores[lvl];
 
-          return isCompleted ? (
-            <div key={lvl} className="level-card locked">
-              <div className="lock-icon">🔒 ჩაკეტილია</div>
-              <h3 className="level-title">{lvl.toUpperCase()}</h3>
-              <p className="level-status">ტესტი დასრულებულია</p>
-              <div className="saved-score-badge">ქულა: {displayScore}</div>
-            </div>
-          ) : (
-            <Link key={lvl} to={`/quiz/${lvl}`} className="level-card active">
-              <div className="unlock-icon">🔓 ღიაა</div>
-              <h3 className="level-title">{lvl.toUpperCase()}</h3>
-              <p className="level-status">შეკითხვები: {levelsData[lvl].length}</p>
-              <span className="btn-start-level">დაწყება</span>
-            </Link>
-          );
-        })}
+            return isCompleted ? (
+              <div key={lvl} className="level-card locked">
+                <div className="lock-icon">🔒 ჩაკეტილია</div>
+                <h3 className="level-title">{lvl.toUpperCase()}</h3>
+                <p className="level-status">ტესტი დასრულებულია</p>
+                <div className="saved-score-badge">ქულა: {displayScore}</div>
+              </div>
+            ) : (
+              <Link key={lvl} to={`/quiz/${lvl}`} className="level-card active">
+                <div className="unlock-icon">🔓 ღიაა</div>
+                <h3 className="level-title">{lvl.toUpperCase()}</h3>
+                <p className="level-status">
+                  შეკითხვები: {levelsData[lvl].length}
+                </p>
+                <span className="btn-start-level">დაწყება</span>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <div className="titleee">
+        <h2>Community Levels</h2>
+        <div className="levels-grid">
+          {Object.keys(CustomLevelData).map((lvl) => {
+            const isCompleted = scores[lvl] !== undefined;
+            const displayScore = scores[lvl];
+
+            return isCompleted ? (
+              <div key={lvl} className="level-card locked">
+                <div className="lock-icon">🔒 ჩაკეტილია</div>
+                <h3 className="level-title">{lvl.toUpperCase()}</h3>
+                <p className="level-status">ტესტი დასრულებულია</p>
+                <div className="saved-score-badge">ქულა: {displayScore}</div>
+              </div>
+            ) : (
+              <Link key={lvl} to={`/quiz/${lvl}`} className="level-card active">
+                <div className="unlock-icon">🔓 ღიაა</div>
+                <h3 className="level-title">{lvl.toUpperCase()}</h3>
+                <p className="level-status">
+                  შეკითხვები: {CustomLevelData[lvl].length}
+                </p>
+                <span className="btn-start-level">დაწყება</span>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ Frontend/src/customLevelData.js
@@ -0,0 +1,74 @@
+const customLevelData = {
+  "Popular Games": [
+    {
+      question: "რა არის ყველაზე გადმოწერილი ვიდეო თამაში მსოფლიოში?",
+      answers: ["Minecraft", "Warzone", "Geometry Dash", "PUBG mobile"],
+      correctAnswer: "Minecraft",
+      timeToThink: 15,
+      maxScore: 20,
+    },
+    {
+      question: "რამდენი მეხსიერება არის რეკომენდებული დიდი ტევდიანი თამაშების გადმოსაწერად?",
+      answers: ["2GB RAM", "32GB RAM", "16GB RAM", "8GB RAM"],
+      correctAnswer: "32GB RAM",
+      timeToThink: 15,
+      maxScore: 30,
+    },
+    {
+      question: "რა არის ყველაზე პოპულრული ROBLOX თამაში?",
+      answers: [
+        "Adopt Me",
+        "Tower of Hell",
+        "bloxfruits",
+        "Brookhaven RP",
+      ],
+      correctAnswer: "",
+      timeToThink: 15,
+      maxScore: 20,
+    },
+    {
+      question: "რა არის ყველაზე პოპულარული mobile თამაში მსოფლიოში?",
+      answers: [
+        "Clash of Clans",
+        "Mobile Legends",
+        "PUBG Mobile",
+        "Free Fire",
+      ],
+      correctAnswer: "PUBG Mobile",
+      timeToThink: 15,
+      maxScore: 20,
+    },
+    {
+      question: "რომელი თამაში არის ყველაზე გაყიდვადი ვიდეო თამაში ისტორიაში?",
+      answers: [
+        "Minecraft",
+        "GTA V",
+        "horizon zero dawn",
+        "Wii Sports",
+      ],
+      correctAnswer: "Minecraft",
+      timeToThink: 15,
+      maxScore: 20,
+    },
+    {
+      question:
+        "რა არის ყველაზე ყველაზე პოპულარული Geometry Dash ტური?",
+      answers: ["ClubStep", "ReTraY", "Dash", "Stereo Madness"],
+      correctAnswer: "Stereo Madness",
+      timeToThink: 15,
+      maxScore: 40,
+    },
+    {
+      question: "რა არის Maincraft-ის ყველაზე ახალი ვერსია?",
+      answers: [
+        "1.21.9",
+        "1.27",
+        "1.26",
+        "1.20.4",
+      ],
+      correctAnswer: "1.26",
+      timeToThink: 13,
+      maxScore: 20,
+    },
+  ],
+};
