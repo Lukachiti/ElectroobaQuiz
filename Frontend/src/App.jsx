6d6087927edaa225af26b2f2db4bf4eec5169216
Lukachiti <chitidze.luka@outlook.com>
2026-06-13 14:26:09 +0400

    comm

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,16 +6,30 @@ import QuizScreen from "./QuizScreen";
 import CreateLevelScreen from "./CreateLevelScreen";
 import AdminScreen from "./AdminScreen";
 
+// 1. IMPORT YOUR AUTH SCREENS HERE
+import LoginScreen from "./LoginScreen";       // Change path/name if different
+import RegisterScreen from "./RegisterScreen"; // Change path/name if different
+
 export default function App() {
   return (
     <Router>
-      <Navbar /> {/* Displayed consistently on all viewpoints */}
-      <Routes>
-        <Route path="/" element={<DashboardScreen />} />
-        <Route path="/quiz/:levelId" element={<QuizScreen />} />
-        <Route path="/create-level" element={<CreateLevelScreen />} />
-        <Route path="/admin" element={<AdminScreen />} />
-      </Routes>
+      {/* Structural wrapper ensures layout styling doesn't break */}
+      <div className="app-layout-wrapper">
+        <Navbar /> 
+        
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<DashboardScreen />} />
+            <Route path="/quiz/:levelId" element={<QuizScreen />} />
+            <Route path="/create-level" element={<CreateLevelScreen />} />
+            <Route path="/admin" element={<AdminScreen />} />
+            
+            {/* 2. ADD THE MISSING ROUTES HERE */}
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
