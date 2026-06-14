6e475a7bc61a4965d24c193abdd8c68f699b6094
Lukachiti <chitidze.luka@outlook.com>
2026-06-14 15:27:34 +0400

    commit

--- Frontend/src/Navbar.jsx
+++ Frontend/src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
+import "./Navbar.css";
 
 export default function Navbar() {
   const auth = useContext(AuthContext);
